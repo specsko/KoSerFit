f98750c217f0652861cd810e9a34578ff692549f
specsko <65127259+specsko@users.noreply.github.com>
2025-09-01 14:45:10 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
-
 plugins {
-    id("com.android.application")
-    kotlin("android")
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
 android {
