c3e91cbcb3a1d76b600eea54af7ee4211f63f7e9
specsko <65127259+specsko@users.noreply.github.com>
2025-09-01 14:28:39 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,9 +42,12 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.2")
 
-    implementation(platform("androidx.compose:compose-bom:2024.09.00"))
+    // Было: platform("androidx.compose:compose-bom:2024.09.00")
+    implementation(platform("androidx.compose:compose-bom:2024.06.00")) // ✅ Compose 1.6.x
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
+
